53b66aef3b8aeb21ef1165b29fe42060ff6b3ed7
TuttiFrutti7 <daces777@gmail.com>
2024-07-30 16:50:26 +0300

    added all the needed classes and made shipping form fill function

--- cypress/e2e/coe-webstore/e2e.cy.js
+++ cypress/e2e/coe-webstore/e2e.cy.js
@@ -1,5 +1,4 @@
 import Global from '../../pageElements/Global';
-import Login from '../../pageElements/Login';
 import Home from '../../pageElements/Home'
 import Checkout from '../../pageElements/Checkout';
 import Cart from '../../pageElements/Cart';
@@ -15,45 +14,53 @@ describe('End to end functionality', () => {
         cy.visit('/');
     });
 
-    it('logs in', () => {});
-
     it('selects product', () => {
       Home.elements.productLink().first().click();
       Product.elements.description().should('be.visible');
     });
 
     it('adds product to a cart', () => {
-      cy.visit('https://coe-webstore.tdlbox.com/products/t-shirt');
+      cy.visit('/products/t-shirt');
       Product.elements.addProductButton().should('not.be.enabled')
-      Product.elements.productOptions('M').click();
+      Product.elements.productOptions('XL').click();
       Product.elements.productOptions('White').click();
       Product.elements.addProductButton().should('be.enabled').click();
     });
 
     it('goes to checkout', () => {
-      cy.visit('https://coe-webstore.tdlbox.com/cart');
+      cy.visit('/cart');
       Cart.elements.checkoutButton().click();
+      cy.contains('Shipping Address');
     });
 
+    /* TODO:
+      - add countdown for buttons to finish loading
+    */
     it('fills in checkout', () => {
-      cy.visit('https://coe-webstore.tdlbox.com/checkout?step=address');
-      // Enter shipping address
-      cy.getByTestId('shipping-first-name-input').clear().type('Bob');
-      cy.getByTestId('shipping-last-name-input').clear().type('Bob');
-      cy.getByTestId('shipping-address-input').clear().type('Traktoru iela 23423');
-      cy.getByTestId('shipping-postal-code-input').clear().type('LV-2121');
-      cy.getByTestId('shipping-city-input').clear().type('Riga');
-      cy.getByTestId('shipping-country-select').select('lv').should('have.value', 'lv');
-      cy.getByTestId('shipping-email-input').clear().type('bob@inbox.lv');
-      // The rest of checkout
+      cy.visit('/checkout?step=address');
+      // enters shipping information
+      //Checkout.elements.shippingFirstNameInput().clear().type('Bob');
+      //Checkout.elements.shippingLastNameInput().clear().type('Dragon');
+      //Checkout.elements.shippingAddressInput().clear().type('Traktoru iela 34');
+      //Checkout.elements.shippingPostalCodeInput().clear().type('LV-4200')
+      //Checkout.elements.shippingCityInput().clear().type('Riga');
+      //Checkout.elements.shippingCountrySelect('lv').should('have.value', 'lv');
+      //Checkout.elements.shippingEmailInput().clear().type('bob@inbox.lv');
+
+      // Trying to automate the shipping form
+      Checkout.fillShipping();
       Checkout.elements.continueToDeliveryButton().click();
+
+      // the rest
       Checkout.elements.deliveryOption('FakeEx Express').click();
       Checkout.elements.continueToPaymentButton().click();
       Checkout.elements.continueToReview().click();
       Checkout.elements.placeOrder().click();
+      cy.contains('Your order was placed successfully');
     });
 
     it('logs out', () => {
       Global.navigateSideBar.logout();
+      cy.url().should('include', '/sign-in');
     });
 })
