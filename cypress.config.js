8671a2026e1f2f2817edffe518d116888e371873
TuttiFrutti7 <daces777@gmail.com>
2024-07-30 09:46:25 +0300

    fixed incorrect format in cypres.config.js file

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-<<<<<<< HEAD
   env: {
     username: 'bob@inbox.lv',
     password: 'password'
@@ -9,15 +8,5 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://coe-webstore.tdlbox.com/',
     setupNodeEvents(on, config) {},
-=======
-e2e: {
-  env: {
-    username: 'bob@inbox.lv',
-    password: 'password'
-  },
-  e2e: {
-    baseUrl: 'https://coe-webstore.tdlbox.com/',
-    setupNodeEvents(on, config) {},
->>>>>>> main
   },
 });
