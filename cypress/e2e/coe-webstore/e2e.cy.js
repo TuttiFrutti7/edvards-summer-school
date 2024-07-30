efa62c20e70fe5344d970b834fd9640dbc043a1f
TuttiFrutti7 <daces777@gmail.com>
2024-07-30 14:39:07 +0300

    added some pageElement classes and used them for e2e full cycle testing

--- cypress/e2e/coe-webstore/e2e.cy.js
+++ cypress/e2e/coe-webstore/e2e.cy.js
@@ -1,14 +1,17 @@
 import Global from '../../pageElements/Global';
-import Login from '../../pageElements/Login'
+import Login from '../../pageElements/Login';
 import Home from '../../pageElements/Home'
+import Checkout from '../../pageElements/Checkout';
+import Cart from '../../pageElements/Cart';
+import Product from '../../pageElements/Product';
 
 describe('End to end functionality', () => {
 
-    const username = Cypress.env('username');
-    const password = Cypress.env('password');
+    const USERNAME = Cypress.env('username');
+    const PASSWORD = Cypress.env('password');
 
     beforeEach(() => {
-        cy.login(username, password);
+        cy.login(USERNAME, PASSWORD);
         cy.visit('/');
     });
 
@@ -16,33 +19,38 @@ describe('End to end functionality', () => {
 
     it('selects product', () => {
       Home.elements.productLink().first().click();
-      cy.getByTestId('product-description').should('be.visible', 'visible');
+      Product.elements.description().should('be.visible');
     });
 
     it('adds product to a cart', () => {
       cy.visit('https://coe-webstore.tdlbox.com/products/t-shirt');
-      cy.getByTestId('add-product-button').should('not.be.enabled');
-      cy.getByTestId('product-options').contains('M').click();
-      cy.getByTestId('product-options').contains('White').click();
-      cy.getByTestId('add-product-button').should('be.enabled');
-      cy.getByTestId('add-product-button').click();
+      Product.elements.addProductButton().should('not.be.enabled')
+      Product.elements.productOptions('M').click();
+      Product.elements.productOptions('White').click();
+      Product.elements.addProductButton().should('be.enabled').click();
     });
 
-    it('checks out', () => {
-      cy.getByTestId('nav-cart-link').click();
-      cy.getByTestId('checkout-button').click();
-      cy.getByTestId('shipping-first-name-input').type('Bob');
-      cy.getByTestId('shipping-last-name-input').type('Bob');
-      cy.getByTestId('shipping-address-input').type('Traktoru iela 23423');
-      cy.getByTestId('shipping-postal-code-input').type('LV-2121');
-      cy.getByTestId('shipping-city-input').type('Riga');
+    it('goes to checkout', () => {
+      cy.visit('https://coe-webstore.tdlbox.com/cart');
+      Cart.elements.checkoutButton().click();
+    });
+
+    it('fills in checkout', () => {
+      cy.visit('https://coe-webstore.tdlbox.com/checkout?step=address');
+      // Enter shipping address
+      cy.getByTestId('shipping-first-name-input').clear().type('Bob');
+      cy.getByTestId('shipping-last-name-input').clear().type('Bob');
+      cy.getByTestId('shipping-address-input').clear().type('Traktoru iela 23423');
+      cy.getByTestId('shipping-postal-code-input').clear().type('LV-2121');
+      cy.getByTestId('shipping-city-input').clear().type('Riga');
       cy.getByTestId('shipping-country-select').select('lv').should('have.value', 'lv');
       cy.getByTestId('shipping-email-input').clear().type('bob@inbox.lv');
-      cy.getByTestId('submit-address-button').click();
-      cy.getByTestId('delivery-option-radio').contains('FakeEx Express').click();
-      cy.getByTestId('submit-delivery-option-button').click();
-      cy.getByTestId('submit-payment-button').should('be.visible').click();
-      cy.getByTestId('submit-order-button').should('be.visible').click();
+      // The rest of checkout
+      Checkout.elements.continueToDeliveryButton().click();
+      Checkout.elements.deliveryOption('FakeEx Express').click();
+      Checkout.elements.continueToPaymentButton().click();
+      Checkout.elements.continueToReview().click();
+      Checkout.elements.placeOrder().click();
     });
 
     it('logs out', () => {
